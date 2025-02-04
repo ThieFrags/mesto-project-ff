1fb1e480efba3606859dbeb49181a4db911a9ab0
Artem Charugin <charuginartem@yandex.ru>
2025-02-04 16:34:01 +0500

    Сделано добавление карточек и кнопка удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+import {initialCards} from './cards.js';
 
-// @todo: DOM узлы
+let content = document.querySelector('.content');
+let cardsList = document.querySelector('.places__list');
+let cards = document.querySelector('.card')
+let deleteButton = document.querySelector('.card__delete-button');
+
+
+function deleteButtonListener(cardElement) {
+  // @todo: Функция удаления карточки
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    cardElement.remove();
+  })
+}
 
 // @todo: Функция создания карточки
+function addCard(name, link) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+
+  deleteButtonListener(cardElement);
+
+  cardsList.appendChild(cardElement);
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards(cards) {
+  cards.forEach(card => addCard(card.name, card.link));
+}
+
+renderCards(initialCards);
